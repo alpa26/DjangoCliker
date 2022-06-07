9739419e724819fe3bb2d1cabadd2c86bacd14ce
panovalex777 <Guitar5712>
2022-06-07 13:25:42 +0500

    add DRF registration model/Rest Framework

--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm
+from rest_framework.views import APIView
 
 def index(request):
     return HttpResponse('index page')
@@ -11,32 +12,33 @@ def index(request):
 def test(request):
     return HttpResponse('test page')
 
-def register(request): 
-    if request.method == 'POST': 
+class Register(APIView):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'register.html', {'form': form})
+    def post(self, request):
         form = UserForm(request.POST) 
         if form.is_valid(): 
             user = form.save() 
             login(request, user) 
             return redirect('index')
+        return render(request, 'register.html', {'form': form})
 
-        return render(request, 'register.html', {'form': form}) 
-     
-    form = UserForm() 
-    return render(request, 'register.html', {'form': form})
 
-def user_login(request): 
-    form = UserForm()
-   
-    if request.method == 'POST': 
+class User_login(APIView):
+    def get(self, request):
+        form = UserForm()
+        return render(request, 'login.html', {'form': form})
+    def post(self, request):
+        form = UserForm()
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password')) 
         if user:
             login(request, user) 
             return redirect('index')
-
         return render(request, 'login.html', {'form': form, 'invalid': True})
 
-    return render(request, 'login.html', {'form': form})
 
+@login_required
 def user_logout(request):
     logout(request)
     return redirect('login')
--- cliker/settings.py
+++ cliker/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'frontend',
+    'rest_framework',
+    #'api',
 ]
 
 MIDDLEWARE = [
